9420ddeec6b4ea4240dccd725e4cbb451b85cd4e
BMISYSTEM <baironmenesesidarraga.990128@gmail.com>
2026-05-15 08:15:44 -0500

    se cambia el color naranja a amarillo

--- components/Visitanos.tsx
+++ components/Visitanos.tsx
@@ -5,15 +5,15 @@ import React from 'react'
 export const Visitanos = () => {
   return (
     <section className='w-full md:h-screen min-h-screen h-auto relative flex items-center'>
-       <div className='w-full h-full bg-red-500 absolute'>
+       <div className='w-full h-full  absolute'>
             <Image src={"/fabrica.png"} alt='imagen de fabrica' width={2000} height={2000} className='w-full h-full object-cover'/>
         </div>
         <div className='w-full h-full absolute  bg-linear-to-r from-black to-transparent'></div>
          <div className='md:w-100 w-full items-start absolute left-10 flex flex-col gap-2 '>
-            <span className='text-xl uppercase text-amber-500'>Nuestra planta</span>
+            <span className='text-xl uppercase text-[#FFD230]'>Nuestra planta</span>
             <span className='md:text-6xl text-3xl font-semibold text-white '>Producción <br/> que construye <br/> confianza <span className='text-amber-500'>.</span></span>
             <span className='md:text-black text-white'>Tecnología, control y experiencia <br/>para entregar prefabricados de<br/> alta calidad en cada proyecto.</span>
-            <div className='w-20 h-0.5 bg-amber-500'></div>
+            <div className='w-20 h-0.5 bg-[#FFD230]'></div>
 
             <div className='flex flex-row gap-5 mt-10 items-center  '>
                 <MapPin size={30} color='orange'/>
@@ -25,7 +25,7 @@ export const Visitanos = () => {
             <div className='mt-10 flex md:flex-row flex-col gap-5 '>
                 <button onClick={()=>{
                     window.open("https://maps.app.goo.gl/Bvh6b8ZnfapyaqbS9","_blanck")
-                }} className='px-5 py-2 flex flex-row gap-5 items-center bg-amber-500 rounded-2xl'>
+                }} className='px-5 py-2 flex flex-row gap-5 items-center bg-[#FFD230] rounded-2xl'>
                     <ArrowRight size={20}/>
                     <span>Abrir Ubicación</span>
                 </button>
